b547aff8c3b6722ad96a2595d2c7d3192c1b65cb
KMart <ken.mart.d@gmail.com>
2024-12-12 18:11:39 -0500

    Hello windows (not quite) actually works!

--- wWinMain.cpp
+++ wWinMain.cpp
@@ -1,7 +1,14 @@
 #include <Windows.h>
 #include <iostream>
+#include <string>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
     std::wcout << L"Hello, Windows!" << std::endl;
     return 0;
 }
+
+int main() {
+    return wWinMain(GetModuleHandle(nullptr), nullptr, GetCommandLineW(), SW_SHOWNORMAL);
+}
+
+
